cdc8cb26755232eb539744749c152ad667a63744
ww <qingfenghuohu@126.com>
2020-03-19 18:58:50 +0800

    init

--- main.go
+++ main.go
@@ -2,20 +2,18 @@ package main
 
 import (
 	"github.com/gin-gonic/gin"
-	"websocket/src/demo"
+	//"./websocket/src/demo"
 )
 
-
-
 func main() {
-	bindAddress := "localhost:2303"
-	router := gin.Default()
-	router.LoadHTMLGlob("src/templates/*")
-	router.StaticFile("/favicon.ico", "src/assets/favicon.ico")
-	router.GET("/ping/:code", demo.Ping)
-	router.GET("/ping2/:code", demo.Ping2)
-	router.GET("/echo", demo.Echo)
-	router.GET("/write/:code/:title/:body", demo.Write)
-	router.GET("/", demo.WebIndex)
-	router.Run(bindAddress)
+	//bindAddress := "localhost:2303"
+	//router := gin.Default()
+	//router.LoadHTMLGlob("src/templates/*")
+	//router.StaticFile("/favicon.ico", "src/assets/favicon.ico")
+	//router.GET("/ping/:code", demo.Ping)
+	//router.GET("/ping2/:code", demo.Ping2)
+	//router.GET("/echo", demo.Echo)
+	//router.GET("/write/:code/:title/:body", demo.Write)
+	//router.GET("/", demo.WebIndex)
+	//router.Run(bindAddress)
 }
